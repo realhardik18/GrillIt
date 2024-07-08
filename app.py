2b71b0e2028595b557d651cd73d8b3a64456a04e
realhardik18 <realhardik18@gmail.com>
2024-07-08 19:14:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import streamlit as st
 from helpers import Roast
 
+key=st.secrets['API_KEY']
+
 st.title('Welcome To GrillIt🔥')
 st.write('Made By [realhardik18](https://x.com/realhardik18) | [Support GrillIt](https://buymeacoffee.com/realhardik18)')
 username = st.text_input(label="Enter Reddit Username")
 
 if st.button("Submit"):    
     if username:        
-        roast=Roast(username,st.secrets['API_KEY'])
+        roast=Roast(username,key)
         st.write(roast)
